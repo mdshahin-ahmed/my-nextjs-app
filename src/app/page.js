af02b6615c340a944eab65054a34439ae6904ded
shahinahmed6057@gmail.com <mdshahinahmed605701@gmail.com>
2024-04-30 09:38:05 +0600

    add: server component

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,12 @@
+import Counter from "@/components/counter/Counter";
+
 const HomePage = () => {
+  // console.log("Hello world");
   return (
     <div>
       <h1 className="text-4xl text-center">Welcome to next js. Home page</h1>
+      <h1 className="text-5xl text-center mt-5">React server component</h1>
+      <Counter />
     </div>
   );
 };
