f211a776b3c92f135c54cb1b03aebbd366c91c99
shahinahmed6057@gmail.com <mdshahinahmed605701@gmail.com>
2024-04-30 22:20:09 +0600

    Add: header and footer layout

--- src/components/counter/Counter.jsx
+++ src/components/counter/Counter.jsx
@@ -8,11 +8,17 @@ const Counter = () => {
     <div>
       <h1>Test</h1>
       <h1>Counter : {counter}</h1>
-      <button className="border p-2" onClick={() => setCounter(counter + 1)}>
+      <button
+        className="btn btn-accent"
+        onClick={() => setCounter(counter + 1)}
+      >
         Increase
       </button>
-      <button className="border p-2" onClick={() => setCounter(counter - 1)}>
-        Decrease
+      <button
+        className="btn btn-accent"
+        onClick={() => setCounter(counter - 1)}
+      >
+        Increase
       </button>
     </div>
   );
